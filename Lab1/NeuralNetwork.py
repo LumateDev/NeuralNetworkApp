a5029de6b3f225cc40cd808704317766deb0b83b
Lumate <sanchous557@mail.ru>
2024-02-09 09:09:51 +0300

    init project

---
+++ Lab1/NeuralNetwork.py
@@ -0,0 +1,10 @@
+from random import random
+from typing import List, Any
+
+
+class NeuralNetwork:
+
+    def __init__(self):
+        pass
+
+
---
+++ Lab1/Neuron_WTA.py
@@ -0,0 +1,9 @@
+from random import random
+
+
+class Neuron_WTA:
+
+    def __init__(self):
+        pass
+
+
